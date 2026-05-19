65a35fa66a7f184df04cc3c9172a997666bc4812
Anjansuman <anjansuman80@gmail.com>
2026-05-19 22:02:31 +0530

    added server packages.

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello" }));
+
+    let port = "10.238.187.81:3000";
+
+    let listener = tokio::net::TcpListener::bind(port).await.unwrap();
+
+    println!("your server is started at {port}");
+
+    axum::serve(listener, app).await.unwrap();
 }
